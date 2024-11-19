d35f9a5f343c5b97e547babeee675545c1006eeb
AnyamaneePloy <anyamanee@techgateway.co.th>
2024-11-19 11:59:19 +0700

    Cleaned up repository to respect .gitignore

--- test_athena_query_lib/test_myfunctions.py
+++
@@ -1,9 +0,0 @@
-# Import the haversine function from the package
-from athena_query_lib import haversine
-
-# Test the function
-lon1, lat1 = 77.5946, 12.9716  # Bangalore
-lon2, lat2 = 72.8777, 19.0760  # Mumbai
-
-distance = haversine(lon1, lat1, lon2, lat2)
-print(f"The distance between Bangalore and Mumbai is approximately {distance:.2f} kilometers.")
